b72a149b3f03a68c1dce76437e19b5f05be1b777
Rabea Gleissner <rabeagleissner@gmail.com>
2022-03-22 16:32:53 +0800

    Don't couple model creation to Author model

--- lib/json_file_reader.rb
+++ lib/json_file_reader.rb
@@ -4,7 +4,7 @@ class JsonFileReader
   def self.read_and_create(file_path, model)
     json_data = JSON.parse(File.read(file_path))
     json_data.map do |data|
-      model.new(data["name"], data["doi"])
+      model.new(*data.values)
     end
   end
 end
